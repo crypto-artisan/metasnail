e81bdd1658e6379fbe2da088a25e9ef6e5fb46d3
hightecular95908 <zulhelmijohn0218@gmail.com>
2022-02-08 07:42:19 +0800

    intro card

--- src/layouts/landing/intropart/index.tsx
+++ src/layouts/landing/intropart/index.tsx
@@ -1,8 +1,66 @@
 import React from "react";
-import { IntroPartSection } from "./intropart.styled";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { FaPlay } from "react-icons/fa";
 
+import {
+  BtnGetMyDiscount,
+  BtnHowItworks,
+  IntroCard,
+  IntroCardOverlay,
+  IntroImg,
+  IntroOverlay,
+  IntroPartSection,
+} from "./intropart.styled";
+
+import IntroCharacter from "assets/characters/intro_1.png";
 const IntroPart = () => {
-  return <IntroPartSection className="">asdf</IntroPartSection>;
+  return (
+    <IntroPartSection className="relative bg-center bg-no-repeat bg-cover">
+      <IntroOverlay className="absolute w-full h-full top-0 bg-center bg-no-repeat bg-cover bg-fixed" />
+      <div className="grid grid-cols-2">
+        <div>
+          <IntroImg
+            src={IntroCharacter}
+            alt="intro character"
+            className="opacity-90 hover:opacity-100"
+          />
+        </div>
+        <div>
+          <IntroCard className="px-24 py-10 w-fit relative">
+            <IntroCardOverlay className="absolute top-0 left-0 w-full h-full -z-[1] mix-blend-darken" />
+            <h6 className="text-white uppercase font-bold">Metasnails !</h6>
+            <h2 className="text-[#E17B00] text-[40px] font-black">
+              New Generation OF P2E !
+            </h2>
+            <p className="text-white text-[22px] pt-16 pb-5">
+              Get MS coin 1.5X cheaper than launch price !
+            </p>
+            <p className="text-white text-[22px] pb-16">
+              and mint your Heros NFTs before launch !!
+            </p>
+            <div className="flex items-center">
+              <BtnGetMyDiscount className="bg-[#fe4c1c] h-fit text-white uppercase rounded-full text-base relative font-bold flex items-center p-[5px] border-x border-[#fe4c1c] cursor-pointer">
+                <span className="px-4">GET MY DISCOUNT</span>
+                <div className="p-3 bg-white text-[#fe4c1c] rounded-full">
+                  <HiArrowNarrowRight />
+                </div>
+              </BtnGetMyDiscount>
+              <BtnHowItworks className="border-[10px] cursor-pointer rounded-full border-[#ffffff1a]">
+                <div className="bg-white hover:bg-[#00c3ff] rounded-full p-4 text-[#00c3ff] hover:text-white">
+                  <FaPlay />
+                </div>
+              </BtnHowItworks>
+              <p className="pl-3 text-[#A2893A] text-base font-bold leading-7">
+                How It
+                <br />
+                Works
+              </p>
+            </div>
+          </IntroCard>
+        </div>
+      </div>
+    </IntroPartSection>
+  );
 };
 
 export default IntroPart;
--- src/layouts/landing/intropart/intropart.styled.tsx
+++ src/layouts/landing/intropart/intropart.styled.tsx
@@ -1,3 +1,71 @@
 import styled from "styled-components";
 
-export const IntroPartSection = styled.div``;
+import introBG from "assets/hert.jpg";
+import introOverlay from "assets/bg-banner-home7.png";
+import introCard from "assets/introCard.jpg";
+
+export const IntroPartSection = styled.div`
+  background-color: #144281;
+  background-image: url(${introOverlay});
+  transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+  z-index: 1;
+  padding-top: 265px;
+`;
+
+export const IntroOverlay = styled.div`
+  background-image: url(${introBG});
+  opacity: 0.31;
+  mix-blend-mode: overlay;
+  transition: background 0.3s, border-radius 0.3s, opacity 0.3s;
+}`;
+
+export const IntroImg = styled.img`
+  padding-left: 5%;
+  transition: 0.3s all;
+  :hover {
+    transform: scale(0.9);
+  }
+`;
+
+export const IntroCard = styled.div`
+  box-shadow: -9px 0px 16px 0px rgb(0 0 0 / 50%);
+  transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+`;
+
+export const IntroCardOverlay = styled.div`
+  background-image: url(${introCard});
+  opacity: 0.4;
+`;
+
+export const BtnGetMyDiscount = styled.div`
+  contain: content;
+  transition: 0.5s all;
+  box-shadow: 8px 8px 18px 0px rgb(254 76 28 / 30%);
+  :before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+    transition: all 0.5s;
+    opacity: 1;
+    transform: translate(-105%, 0);
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+  :hover {
+    box-shadow: none;
+    :before {
+      transform: translate(0, 0);
+      opacity: 0;
+    }
+  }
+`;
+
+export const BtnHowItworks = styled.div`
+  div {
+    transition: all 0.3s;
+    box-shadow: 0px 0px 10px 0px rgb(0 0 0 / 50%);
+  }
+`;
