156a8356dbf392410f31d3cf02d79dcb4f14060a
hightecular95908 <zulhelmijohn0218@gmail.com>
2022-02-08 00:48:21 +0800

    feat: create landing page

---
+++ src/layouts/landing/intropart/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { IntroPartSection } from "./intropart.styled";
+
+const IntroPart = () => {
+  return <IntroPartSection className="">asdf</IntroPartSection>;
+};
+
+export default IntroPart;
---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import IntroPart from "layouts/landing/intropart";
+
+const LandingPage = () => {
+  return (
+    <div className="w-full">
+      <IntroPart />
+    </div>
+  );
+};
+
+export default LandingPage;
