3bd0dd6b3ce83204a5878062b87f79030bcf4350
hightecular95908 <zulhelmijohn0218@gmail.com>
2022-02-09 09:55:14 +0800

    fix: build issues

--- src/layouts/footer/footer.styled.tsx
+++ src/layouts/footer/footer.styled.tsx
@@ -9,18 +9,7 @@ export const FooterDiv = styled.div`
       border-bottom: 1.5px solid #fff;
     }
   }
-  .bottom-btn {
-    :hover {
-      filter: brightness(115%) contrast(100%) saturate(140%) blur(0px)
-        hue-rotate(0deg);
-      animation-name: elementor-animation-pulse-grow;
-      animation-duration: 0.3s;
-      animation-timing-function: linear;
-      animation-direction: alternate;
-    }
-    filter: brightness(100%) contrast(100%) saturate(200%) blur(0px)
-      hue-rotate(176deg);
-  }
+
   @keyframes elementor-animation-pop {
     0% {
       transform: scale(1);
