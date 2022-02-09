adf882514ff35b91f0fc4edccff3afdb8110ef42
hightecular95908 <zulhelmijohn0218@gmail.com>
2022-02-10 02:12:25 +0800

    feat: add link to social button

--- src/components/socialbuttongroup/index.tsx
+++ src/components/socialbuttongroup/index.tsx
@@ -6,18 +6,38 @@ import { SocialDiv } from "./social.styled";
 const SocialButtonsGroup = () => {
   return (
     <SocialDiv className="fixed right-0 z-10 bottom-[20%]">
-      <div className="w-[50px] h-[50px] rounded-full border-[3px] border-white bg-[#3b5998] text-white text-[25px] flex justify-center items-center cursor-pointer">
+      <a
+        href="https://facebook.com"
+        rel="noreferrer"
+        target="_blank"
+        className="w-[50px] h-[50px] rounded-full border-[3px] border-white bg-[#3b5998] text-white text-[25px] flex justify-center items-center cursor-pointer"
+      >
         <FaFacebook />
-      </div>
-      <div className="w-[50px] h-[50px] rounded-full border-[3px] border-white bg-[#1da1f2] text-white text-[25px] flex justify-center items-center cursor-pointer">
+      </a>
+      <a
+        href="https://twitter.com"
+        rel="noreferrer"
+        target="_blank"
+        className="w-[50px] h-[50px] rounded-full border-[3px] border-white bg-[#1da1f2] text-white text-[25px] flex justify-center items-center cursor-pointer"
+      >
         <FaTwitter />
-      </div>
-      <div className="w-[50px] h-[50px] rounded-full border-[3px] border-white bg-[#1da1f2] text-white text-[25px] flex justify-center items-center cursor-pointer">
+      </a>
+      <a
+        href="https://telegram.com"
+        rel="noreferrer"
+        target="_blank"
+        className="w-[50px] h-[50px] rounded-full border-[3px] border-white bg-[#1da1f2] text-white text-[25px] flex justify-center items-center cursor-pointer"
+      >
         <FaTelegram />
-      </div>
-      <div className="w-[50px] h-[50px] rounded-full border-[3px] border-white bg-[#ff4500] text-white text-[25px] flex justify-center items-center cursor-pointer">
+      </a>
+      <a
+        href="https://linkedin.com"
+        rel="noreferrer"
+        target="_blank"
+        className="w-[50px] h-[50px] rounded-full border-[3px] border-white bg-[#ff4500] text-white text-[25px] flex justify-center items-center cursor-pointer"
+      >
         <FaReddit />
-      </div>
+      </a>
     </SocialDiv>
   );
 };
